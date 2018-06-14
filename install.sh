fd0edab2352efc09d60c6478d4531a14bf3acf75
activdev <mujtahid@outlook.com>
2018-06-14 20:44:57 +0200

    v2

--- install.sh
+++ install.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
-sudo touch /var/swap.img
-sudo chmod 600 /var/swap.img
-sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-mkswap /var/swap.img
-sudo swapon /var/swap.img
-sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+
+COIN='https://github.com/innovacoin/innova/releases/download/12.1.10/linux_x64.tar.gz'
+
 sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install nano htop git -y
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common -y
-sudo apt-get install libboost-all-dev -y
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get update -y
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-mkdir /root/temp
-sudo git clone https://github.com/innovacoin/innova /root/temp
-chmod -R 755 /root/temp
-cd /root/temp
-./autogen.sh
-./configure
-sudo make
-sudo make install
-cd
 mkdir /root/innova
 mkdir /root/.innovacore
-cp /root/temp/src/innovad /root/innova
-cp /root/temp/src/innova-cli /root/innova
-chmod -R 755 /root/innova
-chmod -R 755 /root/.innovacore
+cd /root/innova
+wget -q $COIN
+tar xvzf linux_x64.tar.gz
 sudo apt-get install -y pwgen
 GEN_PASS=`pwgen -1 20 -n`
 echo -e "rpcuser=innovauser\nrpcpassword=${GEN_PASS}\nrpcport=14519\nport=14520\nlisten=1\nmaxconnections=256" > /root/.innovacore/innova.conf
@@ -58,5 +37,7 @@ echo "*/30 * * * * /root/mnchecker/mnchecker --currency-handle=\"innova\" --curr
 crontab tempcron
 rm tempcron
 #set masternodeprivkey
+NODEIP=$(curl -s4 icanhazip.com)
+echo "VPS ip: $NODEIP"
 echo "Masternode private key: $masternodekey"
 echo "Job completed successfully"
